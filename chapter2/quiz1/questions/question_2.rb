bf2b136407818fd0dc74bfc16745cdb3134719cd
Richard <aagbajdos@gmail.com>
2019-10-22 13:34:41 +0100

    deleted files

--- chapter2/quiz1/questions/question_2.rb
+++
@@ -1,4 +0,0 @@
-# Write a program that multiplies `TWELVE` by `FOURTEEN` by `SIXTEEN`
-# and `puts`es the result.
-puts 12 * 14 * 16
-
--- chapter4/quiz1/questions/question_2.rb
+++
@@ -1,7 +0,0 @@
-# Write a program that adds together all the integers from `1` to
-# `250` (inclusive) and `puts`es the total.
-i = 250
-    b = (i*(i+1))/2
-    puts b
-
-
