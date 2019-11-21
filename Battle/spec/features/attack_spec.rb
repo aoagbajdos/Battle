ba9348a79bb24309c23ab00371dc042f0db6510b
Richard <aagbajdos@gmail.com>
2019-11-21 15:56:43 +0000

    Implementing hit points

--- Battle/spec/features/attack_spec.rb
+++ Battle/spec/features/attack_spec.rb
@@ -1,8 +1,11 @@
 
 feature 'Attacking' do
+  # 'attack Player 2' scenario omitted
   scenario 'attack Player 2' do
     sign_in_and_play
     click_button 'Attack'
+    click_link 'OK'
+    expect(page).not_to 
     expect(page).to have_content 'Dave attacked Mittens'
   end
 end
